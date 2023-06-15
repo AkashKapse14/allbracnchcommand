669a9771c702fd96b12a80d4aa68c84b164d4f7d
akashkapse149@gmail.com <aryakapse@gmail.com>
2023-06-15 10:41:44 +0530

    modified

--- src/DevClassTest.java
+++ src/DevClassTest.java
@@ -2,5 +2,8 @@ public class DevClassTest {
     public static void main(String[] args) {
 
         System.out.println("This is test class");
+
+        //the change in github file
+        System.out.println("change the file");
     }
 }
