19bfc4e43d0956950b28f5f11228aafb7d672645
akashkapse149@gmail.com <aryakapse@gmail.com>
2023-06-15 10:56:25 +0530

    akash/akash1

--- src/DevClass.java
+++ src/DevClass.java
@@ -2,5 +2,8 @@ public class DevClass {
 
     public static void main(String[] args) {
         System.out.println("This is devclass");
+
+        //this akash file
+        System.out.println("akash/akash1");
     }
 }
